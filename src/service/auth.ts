2611ae311243b4b140553ffaf4ff788847ceebef
trungluong <trungluong>
2023-07-07 10:21:05 +0700

    fix

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -2,6 +2,7 @@ import axiosClient from "./axios";
 import {ILoginPayload} from "../interface/request";
 
 
+
 export default {
     login : async (data:ILoginPayload) =>{
         return await axiosClient.post(`/auth/login`,data)
